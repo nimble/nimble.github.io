e070920532052461399003f850c826e6527aa9aa
LifeOfJona <LifeOfJona@hotmail.com>
2020-09-15 15:56:37 -0400

    realized i need to use React for my site, rip.

---
+++ js/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';
+import 'ant-design-pro/dist/ant-design-pro.css';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<TextAreaManager />, rootElement);
+ReactDOM.render(<DatePicker />, mountNode);
